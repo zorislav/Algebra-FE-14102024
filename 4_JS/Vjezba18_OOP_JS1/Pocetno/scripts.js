e7d3b1f2b159879e98f438447e8733de02522ea1
Zori <46168195+zorislav@users.noreply.github.com>
2024-09-26 13:35:36 +0200

    Add files via upload

---
+++ 4_JS/Vjezba18_OOP_JS1/Pocetno/scripts.js
@@ -0,0 +1,12 @@
+var character = {
+  name: 'Eleven',
+  show: 'Stranger Things',
+  portrayedBy: 'Millie Bobby Brown'
+}
+
+const quotes = [
+	'I\'m going to my friends. I\'m going home.', 
+	'Pure fuel! PURE FUEL! WOO!', 
+	'See? Zoomer.', 
+	'Bitchin.'
+];
