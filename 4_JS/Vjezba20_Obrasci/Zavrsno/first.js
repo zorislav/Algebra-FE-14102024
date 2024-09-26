5f39bb853485ac9f0ca7710c2b9d2cfdeaef2304
Zori <46168195+zorislav@users.noreply.github.com>
2024-09-26 13:49:20 +0200

    Add files via upload

---
+++ 4_JS/Vjezba20_Obrasci/Zavrsno/first.js
@@ -0,0 +1,14 @@
+export const Exercise = (function(){   
+  var steps = 0;
+  function increaseSteps(){
+      steps++;
+  }
+  return{
+      getTotalSteps:function(){
+          return steps;
+      },
+      walk: function(){
+          increaseSteps();            
+      }
+  };
+}());
