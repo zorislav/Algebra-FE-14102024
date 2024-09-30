89056e9a7eef94140cabbe4c956b5188a8d7751a
Zori <46168195+zorislav@users.noreply.github.com>
2024-09-30 10:07:59 +0200

    Add files via upload

---
+++ 5_Razvojna_okolina/Vjezba8_Bootstrap/Pocetno/script.js
@@ -0,0 +1,19 @@
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://pokeapi.co/api/v2/pokemon-color/yellow", true);
+
+function fillList() {
+	const data 		= JSON.parse(xhr.response);
+	const source   	= document.getElementById("pokemon-list").innerHTML;
+	const template 	= Handlebars.compile(source);
+	const context 	= { pokemon: data.pokemon_species.slice(0, 20) };
+	const html     	= template(context);
+	
+	document.getElementById('result').innerHTML = html;
+}
+
+xhr.onload = function() {
+	fillList();
+};
+
+xhr.send();
+
