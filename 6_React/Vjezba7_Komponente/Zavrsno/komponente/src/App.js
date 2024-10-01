92f125da0c3a1ac2d9d36e66f3481c46737e984c
Zori <46168195+zorislav@users.noreply.github.com>
2024-10-01 09:26:25 +0200

    Add files via upload

---
+++ 6_React/Vjezba7_Komponente/Zavrsno/komponente/src/App.js
@@ -0,0 +1,13 @@
+import WelcomeFunction from './WelcomeFunction';
+import WelcomeClass from './WelcomeClass';
+
+function App() {
+  return (
+    <>
+      <WelcomeFunction />
+      <WelcomeClass />
+    </>
+  );
+}
+
+export default App;
