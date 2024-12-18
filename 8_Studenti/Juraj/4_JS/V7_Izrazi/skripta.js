5bf2d93a97a5308a009242b3372ce4db9a525f5b
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2024-12-18 18:11:34 +0100

    Update skripta.js

--- 8_Studenti/Juraj/4_JS/V7_Izrazi/skripta.js
+++ 8_Studenti/Juraj/4_JS/V7_Izrazi/skripta.js
@@ -1,15 +1,5 @@
-// for(let i=3; i <= 20; i++){
-//     if (i%9 !== 0) console.log(i);
-// }
+// 1. alternativa
 
-
-// var varOne = 0;
-// for (i = 0; i < 4; i++){
-//     for (j = 0; j < 4; j++){
-//         varOne++;
-//     }
-// }
-// console.log(varOne);
 
 var broj = 3;
 
@@ -22,3 +12,47 @@ while (broj <= 20)
     }
     console.log(broj);
 }
+
+// 2. alternativa
+
+for(let i=3; i <= 20; i++){
+    if (i%9 !== 0) console.log(i);
+}
+
+
+// 2. dio zadatka
+
+var varOne = 0;
+
+for (i = 0; i < 4; i++){
+    for (j = 0; j < 4; j++){
+        varOne++;
+    }
+}
+console.log(varOne);
+
+// Bonus
+
+
+const niz = ['a', 'b', 'c', 'd'];
+
+for (let i=0, i < niz.length; i++) {
+console.log(niz[i]);
+}
+
+
+// for...of
+
+for (const element of niz) {
+    console.log(element);
+}
+
+
+// for...in
+
+const objekt = {a:1, b:2, c:3};
+for(const svojstvo in objekt) {
+    console.log(svojstvo + ' : ' + objekt[svojstvo]);
+}
+
+
