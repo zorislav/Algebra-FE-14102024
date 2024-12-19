0d78ff8b1110a8e95f416ac90d6094ce498bebcc
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2024-12-19 19:15:50 +0100

    up

--- 8_Studenti/Juraj/4_JS/V9_Nizovi/skripta.js
+++ 8_Studenti/Juraj/4_JS/V9_Nizovi/skripta.js
@@ -50,8 +50,11 @@ for (var i=0; i < names.length; i++){
 console.log(names);
 
 
-if (!names[i]) names.splice(i,1);
-console.log(names);
+// if (!names[i]) names.splice(i,1);
+// console.log(names);
+
+
+
 
 // for (var i = 0; i < names.length; i++) {
 //     if (names[i]) {
