151af199ac8f00d22e19abe30d549998591f2a1e
Božo Kokan <123265355+BKOKAN@users.noreply.github.com>
2025-01-09 22:03:14 +0100

    izgled

--- 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V12_Osnove_Programiranja2/Zadatak/script.js
+++ 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V12_Osnove_Programiranja2/Zadatak/script.js
@@ -1,7 +1,8 @@
 var password = '12345';
 var isDuplicate = false;
 
-for (let i = 0; i < password.length; i++) {
+for (let i = 0; i < password.length; i++) 
+{
     for (let j = 0; j < password.length; j++) 
     {
         if (i !== j && password[i] === password[j]) 
