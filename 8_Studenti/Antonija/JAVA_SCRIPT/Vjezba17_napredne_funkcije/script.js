81af667e4a22e2fa909f8d175b2f6b66c3a9d41e
antonija94 <antonija.mandaric1994@gmail.com>
2025-01-14 20:16:48 +0100

    od 14.1

---
+++ 8_Studenti/Antonija/JAVA_SCRIPT/Vjezba17_napredne_funkcije/script.js
@@ -0,0 +1,12 @@
+//1. Pratite kod u scripts.js. Koje su vrijednosti y i z varijabli na kraju programa?
+
+
+
+var x = "John Doe"; 
+function vratiNesto(name) {
+   var x = name;
+   function f() { return x; }
+   return f;
+}
+var y = vratiNesto("Jane Doe");
+var z = vratiNesto()();//2. Od funkcije u prvom zadatku stvorite samopozivajuću anonimnu funkciju koja prima parametar "name". Kreirajte globanu varijablu koju ta funkcija koristi i zadajte joj vrijednost.
