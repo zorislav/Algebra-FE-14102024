d829847d689fb8828b7ddddb49d9ca5c24ebf583
Božo Kokan <bozokokan11@hotmail.com>
2025-01-22 21:00:16 +0100

    Predavanje

--- 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V23_Arrow_funkcija/scripts.js
+++ 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V23_Arrow_funkcija/scripts.js
@@ -10,3 +10,49 @@ const fn1 = (par) => `Dobio sam ${par}`;
 // }
 
 console.log(fn('22'));
+
+// const Osoba = {
+//     ime: 'Ivica',
+//     pozdrav: function(){
+//         console.log('Bok');
+//     }
+// };
+
+// Osoba.pozdrav();
+
+// Arrow funkcija samo sa jednom komandom bez bloka {}
+// // const Osoba = {
+// //     ime: 'Ivica',
+// //     pozdrav: () =>  console.log('Bok')
+// // };
+
+// Krace pisanje koda primjer unutar objekta
+// // const Osoba = {
+// //     ime: 'Ivica',
+// //     pozdrav (){
+// //                 console.log('Bok')
+// //               };
+// // }
+
+
+// Krace pisanje koda
+function Pas(vrsta, boja){
+
+    let myBoja = boja;
+    let myVrsta = vrsta;
+
+    function myGlas (){
+        console.log ("Lajem");      
+    }
+
+    return {    
+       boja,
+       vrsta,
+       myGlas
+    }
+}
+
+const Rex = new Pas("mali", "crni");
+console.log(Rex.myBoja); // undifined mora imati unutar return da nesto vrati inace je samo unutar funkcije || objekta definirani ne izvan...
+console.log(Rex.boja);
+Rex.myGlas();
