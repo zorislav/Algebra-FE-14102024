d16c389f3d1ddd92e9eb9ca50c140d29ae1a3453
Božo Kokan <bozokokan11@hotmail.com>
2025-01-30 17:05:21 +0100

    update predavanja

--- 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V29_Otkrivanje_Pogresaka/script.js
+++ 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/V29_Otkrivanje_Pogresaka/script.js
@@ -5,9 +5,9 @@
 // console.log(brojac);
 // console.log("Kraj");
 
-name = "James";
+var name1 = "James";
 const person = {
-    first: name
+    first: name1
 };
 
 console.log(person);
@@ -15,3 +15,9 @@ console.log(person);
 const sayHelloLinting = (fName) => {
     console.log(`Hello linting, ${fName}`);
 }
+
+// terminal npm install {package} --save-dev samo za development proces u build nece bit ukljucen
+// terminal npm install {package} ukljucuje se u build product
+// netreba node_module file samo package.json onda upisati npm install i npm vuce iz package.json dependencies, dobro je za automatski update verzije
+// netreba node_module file samo package-lock.json onda upisati npm ci - (Clean Install) i npm vuce sve verzije iz package-lock.json, dobro za odrzavanje verzije koja je vec u projektu
+// za uplodanje na git ignorirati node_modules za upload
