15cbf964374eeb0a649e8cbc17de5ad9b9762841
EdinaKolakovic <edina.kolakovic98@gmail.com>
2025-02-01 14:33:27 +0100

    Novi commit

--- 8_Studenti/Edina/4_JS/V25_Todo/skripta.js
+++ 8_Studenti/Edina/4_JS/V25_Todo/skripta.js
@@ -1,8 +1,10 @@
 //s ovom samopozivajucom anonimnom funkcijom izdvajamo naš kod iz GLOBALNOG prostora (stavljamo ga u zaštićeni prostor kao)
 
 (function () {
+  // Novi objekt
   function Todo() {
-    const input = document.querySelector("#input-text");
+    // Vezanje classa iz htmla u scriptu
+    const inputText = document.querySelector("#input-text");
     const addButton = document.querySelector("#input-add");
     const list = document.querySelector("ul");
     const allButton = document.querySelector("#button-all");
@@ -12,53 +14,64 @@
       "#button-clear-completed"
     );
 
+    // Kreiranje funkcije za provjeru input texta od korisnika
     function addListItem() {
-      let text = input.value;
+      let text = inputText.value;
 
-      if (text.trim().lenght !== 0) {
+      if (text.trim().length !== 0) {
+        // kreiraj novi list item
         const newItem = createListItem(text);
-        const list = document.querySelector(".ul-todo");
+        // dodaj novi list item
         list.appendChild(newItem);
-
-        input.value = "";
+        inputText.value = "";
       } else {
-        alert("Please enter todo");
+        alert("Please enter todo!");
       }
     }
 
+    // nakon kreiranja list itema dodavanje texta unutar list itema i postavljanje redostljeda dodanih elementa radi vizualnosti i dodavanje funckija za novo kreirane elemente sa event listenerima
     function createListItem(text) {
       const listItem = document.createElement("li");
       const div = document.createElement("div");
       const intDiv = document.createElement("div");
       div.classList.add("li-container");
       intDiv.classList.add("li-container-int");
+
+      // dodavanje texta nakon user inputa i klika na add
       intDiv.innerText = text;
-      addCheckbox(intDiv);
+      // dodavanje check boxa unutar div elementa koji je parent
+      addCheckBox(intDiv);
+      // kreiranje div elemnta nakon diva koji je parent
       div.appendChild(intDiv);
-      listItem.appendChild(div);
+      // brise odabrani div element pomocu X koji smo stavili kao simulaciju buttona
       addRemoveButton(div);
+      // dodavanje list itema nakon diva
+      listItem.appendChild(div);
 
       return listItem;
     }
 
-    function addCheckbox(element) {
+    // dodavanje check box funkcije sa event listeneron
+    function addCheckBox(element) {
       const checkBox = document.createElement("input");
       checkBox.setAttribute("type", "checkbox");
       checkBox.addEventListener("click", checkListItem);
       element.insertBefore(checkBox, element.firstChild);
     }
 
+    // dodavanje remove button pomocu div elemnta koji ima inner text kao X za simulaciju buttona i event listenera za taj novo kreirani element
     function addRemoveButton(element) {
       const removeButton = document.createElement("div");
       removeButton.innerText = "X";
       removeButton.className = "removeButton";
       removeButton.addEventListener("click", removeListItem);
+
       element.appendChild(removeButton);
     }
 
+    // ako se klikne check box da se stavi linija kroz tekst
     function checkListItem(event) {
       const checkBox = event.target;
-
       if (checkBox.checked) {
         checkBox.parentNode.style.textDecoration = "line-through";
       } else {
@@ -66,79 +79,88 @@
       }
     }
 
-    function removeListItem(event) {
+    // kreiranje eventa za klick na X za removanje tog list itema
+    function removeListItem() {
       const removeButton = event.target;
       removeButton.parentNode.parentNode.remove();
     }
 
+    // prikaz svih list itema
     function showAll() {
-      const listItems = list.getElemenstByTagName("li");
+      const listItems = list.getElementsByTagName("li");
 
-      for (let i = 0; i < listItems.lenght; i++) {
+      for (let i = 0; i < listItems.length; i++) {
         listItems[i].style.display = "";
       }
+
       allButton.disabled = true;
       activeButton.disabled = false;
       completedButton.disabled = false;
     }
 
+    // prikaz samo list itema koji nemaju ukljucen check box
     function showActive() {
-      const listItems = list.getElemenstByTagName("li");
+      const listItems = list.getElementsByTagName("li");
 
-      for (let i = 0; i < listItems.lenght; i++) {
-        const check = listItems[i].getElemenstByTagName("input");
+      for (let i = 0; i < listItems.length; i++) {
+        const check = listItems[i].getElementsByTagName("input");
         if (check[0].checked) {
           listItems[i].style.display = "none";
         } else {
           listItems[i].style.display = "";
         }
       }
+
       allButton.disabled = false;
       activeButton.disabled = true;
       completedButton.disabled = false;
     }
 
+    // prikaz samo list itema koji imaju ukljucen check box
     function showCompleted() {
-      const listItems = list.getElemenstByTagName("li");
+      const listItems = list.getElementsByTagName("li");
 
-      for (let i = 0; i < listItems.lenght; i++) {
-        const check = listItems[i].getElemenstByTagName("input");
+      for (let i = 0; i < listItems.length; i++) {
+        const check = listItems[i].getElementsByTagName("input");
         if (!check[0].checked) {
           listItems[i].style.display = "none";
         } else {
           listItems[i].style.display = "";
         }
       }
+
       allButton.disabled = false;
       activeButton.disabled = false;
       completedButton.disabled = true;
     }
 
+    // dodavanje remove opcije
     function removeAllCompleted() {
       const listItems = list.getElementsByTagName("li");
-
       for (let i = listItems.length - 1; i >= 0; i--) {
-        const check = listItems[i].getElemenstByTagName("input");
-
+        const check = listItems[i].getElementsByTagName("input");
         if (check[0].checked) {
           listItems[i].remove();
         }
       }
     }
 
+    // dodavanje event listener za postojoce sekcije u html dokumentu
     this.addListeners = function () {
       addButton.addEventListener("click", addListItem);
       allButton.addEventListener("click", showAll);
-      activeButton.addEventListener("click", showActive);
       completedButton.addEventListener("click", showCompleted);
+      activeButton.addEventListener("click", showActive);
       clearAllCompletedButton.addEventListener("click", removeAllCompleted);
     };
   }
 
+  // dodavanje novog prototip svojstva unutar postojeceg objekta Todo
   Todo.prototype.init = function () {
     this.addListeners();
   };
 
+  // Kreiranje novog objekta pomocu postojeceg
   const todo = new Todo();
 
   window.addEventListener("load", todo.init()); //kada se dokument loada (kada se skripta učita), izvrsiti ce se funkcija init koja je definirana na prototipu konstruktor funkcije Todo, a koja je i na raspolaganju onda našem objektu todo
