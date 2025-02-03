04178c88ebe71a546772297bedef2fea2fd9ddf1
antonija94 <antonija.mandaric1994@gmail.com>
2025-02-03 17:42:18 +0100

    do 3.2

---
+++ 8_Studenti/Antonija/JAVA_SCRIPT/Vjezba27-pogreske/script.js
@@ -0,0 +1,19 @@
+//console.log("Pocetak");
+//var brojac = 0;
+
+//console.log("Sredina");
+//brojac = brojac + 1;
+//console.log(brojac);
+//console.log("Kraj");
+
+
+name = "james";
+const person = {first: name};
+
+console.log(person);
+
+const sayHelloLinda = (fName) => {
+console.log(`Hello Linda, ${fName}`);
+
+}
+
