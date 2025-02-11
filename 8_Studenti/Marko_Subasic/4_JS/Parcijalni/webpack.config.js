77c492cd5701de3d75ece11d91593a4e1ab7ee48
Marko Šubašić <36752182+SubasicMarko@users.noreply.github.com>
2025-02-11 20:09:12 +0100

    Parcijalni update
    Dodana minifikacija

--- 8_Studenti/Marko_Subasic/4_JS/Parcijalni/webpack.config.js
+++ 8_Studenti/Marko_Subasic/4_JS/Parcijalni/webpack.config.js
@@ -1,40 +1,60 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, 
-    assetModuleFilename: 'images/[hash][ext][query]', 
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      assetModuleFilename: 'images/[hash][ext][query]',
     },
-    compress: true,
-    port: 9000,
-    open: true,
-    hot: true, 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'], 
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i, 
-        type: 'asset/resource', 
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', 
-      filename: 'index.html',
-      inject: 'body',
-    })
-  ]
+      compress: true,
+      port: 9000,
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      })
+    ],
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
