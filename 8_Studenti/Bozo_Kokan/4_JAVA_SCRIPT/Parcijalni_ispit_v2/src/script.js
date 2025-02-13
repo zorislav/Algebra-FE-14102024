11adc3de50599f15b1250fbb3e54afbe981e18fe
Božo Kokan <bozokokan11@hotmail.com>
2025-02-13 20:56:09 +0100

    predavanje

--- 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/Parcijalni_ispit_v2/src/script.js
+++ 8_Studenti/Bozo_Kokan/4_JAVA_SCRIPT/Parcijalni_ispit_v2/src/script.js
@@ -1,4 +1,10 @@
+import { fetchData } from "./data.js";
+import { createElement } from "./render.js";
+
 const inputElement = document.getElementById("searchTerm");
+const songList = document.getElementById("songsList");
+const noSongs = document.getElementById("noSongs");
+const loadingSpinner = document.getElementById("loadingSpinner");
 
 var timeout = null;
 
@@ -11,11 +17,41 @@ export default () => {
 
         clearTimeout(timeout);
 
-        timeout = setTimeout(() => {
-            
-            //api request
+        timeout = setTimeout( async () => {
 
-            console.log(inputElement.value);
+            const inputElementValue = inputElement.value.trim();
+            while(songList.firstChild){
+                songList.removeChild(songList.firstChild);
+            }
+
+            if(inputElement.value){
+
+                // gasimo noSongs
+                noSongs.style.display = "none";
+                
+                // palimo loading spinner
+                loadingSpinner.style.display = "block";
+                var retData = [];
+                retData = await fetchData(inputElementValue);
+
+                //gasimo loading spinner
+                loadingSpinner.style.display = "none";
+
+                if(retData.length != 0){
+                    for (var i in retData){
+                        //api request
+                        const listItemElement = createElement(retData[i]);
+                        songList.appendChild(listItemElement);
+                    }
+                } else {
+                    // palimo noSongs
+                    noSongs.style.display = "block";
+                }
+
+            }  else {
+                // palimo noSongs
+                noSongs.style.display = "block";
+            }     
 
         },800);
     }
