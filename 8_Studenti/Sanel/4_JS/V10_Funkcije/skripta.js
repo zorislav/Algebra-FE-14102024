b4f67c6235e5dd05401310a0f47c6bb367de023d
SanelBihorac <sbihorac85@gmail.com>
2025-02-17 17:25:21 +0100

    Zadaća i zaostali commitovi

---
+++ 8_Studenti/Sanel/4_JS/V10_Funkcije/skripta.js
@@ -0,0 +1,63 @@
+var words = ['quickest', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'];
+
+// 1. Napišite JavaScript funkciju koja prihvaća zadani niz kao parametar i pronalazi najdužu riječ unutar niza.
+
+var word = "";
+
+for (var i = 0; i < words.length; i++) {
+    if (word.length < words[i].length) word = words[i];
+}
+
+console.log(word);
+
+// 2. Vratite tu riječ iz funkcije i spremite u varijablu.
+
+function findLongestWord(niz) {
+
+    var word = "";
+
+    for (var i = 0; i < niz.length; i++) {
+        if (word.length < niz[i].length) word = niz[i];
+    }
+
+    return word;
+}
+
+var najduzaRijec = findLongestWord(words);
+
+console.log(najduzaRijec);
+
+// Bonus
+
+var novaFunkcija = findLongestWord;
+
+console.log(novaFunkcija(words));
+
+// forEach
+
+var mojNiz = [1, 2, 3, 4];
+
+function ispisi(element) {
+    console.log(element);
+}
+
+mojNiz.forEach(ispisi);
+
+mojNiz.forEach(function (el) {
+    console.log(el);
+});
+
+// map 
+
+function novi(br) {
+    return br + 1;
+}
+
+var noviNiz = mojNiz.map(novi);
+
+// var noviNiz = mojNiz.map(function (x) {
+//     return x + 1;
+// });
+
+console.log(mojNiz);
+console.log(noviNiz);
