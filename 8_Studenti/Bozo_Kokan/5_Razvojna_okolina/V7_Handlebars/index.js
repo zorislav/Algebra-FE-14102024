2de336b92af52742b07bfaa56dc32e18c2292f3a
Božo Kokan <bozokokan11@hotmail.com>
2025-02-22 14:09:25 +0100

    1

--- 8_Studenti/Bozo_Kokan/5_Razvojna_okolina/V7_Handlebars/index.js
+++ 8_Studenti/Bozo_Kokan/5_Razvojna_okolina/V7_Handlebars/index.js
@@ -1,32 +1,18 @@
-const source = document.getElementById("hb-template").innerHTML;
-var destination = document.getElementById("hb-result");
-const template = Handlebars.compile(source);
+document.addEventListener("DOMContentLoaded", main);
 
-document.addEventListener("DOMContentLoaded",main);
+function main() {
+  // Dohvati pokemone
 
-function main(){
+  async function getPomemons() {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-color/yellow"
+    );
+    const responseData = await response.json();
 
-    // Dohvati pokemone
-    async function getPokemons(){
+    console.log(responseData);
+  }
 
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow");
-        const responseData = await response.json();
-         
-        return responseData.pokemon_species.slice(0, 20);
-    }
+  // Prikazi pokemone
 
-    // Prikazi pokemone
-
-    function fillList(pokemons){
-        const context = {pokemon: pokemons};
-        const html = template(context);
-        destination.innerHTML = html;
-
-    }
-
-    getPokemons().then(pkmns => {
-        fillList(pkmns);
-    });
-
-    
+  getPomemons();
 }
