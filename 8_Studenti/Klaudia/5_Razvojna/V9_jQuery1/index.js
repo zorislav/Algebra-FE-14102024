fa299179db9316461601cd8448685510ad657125
Klaludia <klaudija15@windowslive.com>
2025-02-25 17:51:58 +0100

    19.2.

---
+++ 8_Studenti/Klaudia/5_Razvojna/V9_jQuery1/index.js
@@ -0,0 +1,40 @@
+const source = document.getElementById("hb-template").innerHTML;
+const destination = document.getElementById("hb-result");
+const template = Handlebars.compile(source);
+
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  // Dohvati pokemone
+
+  async function getPomemons() {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-color/yellow"
+    );
+    const responseData = await response.json();
+
+    return responseData.pokemon_species.slice(0, 20);
+  }
+
+  // Prikazi pokemone
+
+  function fillList(pokemons){
+    console.log(pokemons);
+    const context = { pokemon: pokemons };
+    const html = template(context);
+    destination.innerHTML = html;
+    $('[data-toggle="popover"]').popover();
+
+    afterRender();
+  }
+
+  function afterRender(){
+    $("table th").css("color", "darkBlue");
+    $("table tr:nth-child(even)").addClass("striped");
+  }
+
+   getPomemons().then(pkmns => {
+    fillList(pkmns);
+   });
+}
