a7ab4a389da10b6d7253d97acfe1be912352b165
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-03-03 21:13:01 +0100

    Nakon 3.3. 2. dio

--- 8_Studenti/Zorislav/6_React/V2_Dependency/deps/src/App.js
+++ 8_Studenti/Zorislav/6_React/V2_Dependency/deps/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import randomstring from "randomstring";
+import "./App.css";
 
 function App() {
+  const mojString = randomstring.generate({
+    charset: "alphanumeric",
+    length: 6,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Moj random string je: {mojString}</p>
     </div>
   );
 }
