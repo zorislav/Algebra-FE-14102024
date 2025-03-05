2d946cc681e22f6d292f353c51831992552fa680
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2025-03-05 20:55:57 +0100

    react

---
+++ 8_Studenti/Juraj/6_React/V7_Komponente/komp/src/App.js
@@ -0,0 +1,16 @@
+
+import WelcomeClass from './Components/WelcomeClass';
+import WelcomeFunction from './Components/WelcomeFunction';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <WelcomeClass />
+     <WelcomeFunction />
+    </div>
+  );
+}
+
+export default App;
---
+++ 8_Studenti/Juraj/6_React/V7_Komponente/komp/src/Components/WelcomeClass.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class WelcomeClass extends React.Component {
+
+    render() {
+        // izvršava se kod svakog prikazivanja
+        return <h2>Welcome! Komponenta definirana klasom</h2>;
+    }
+}
