48ac568c713338b1f5dd1c28144febc1d6f36eaa
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-03-05 21:02:19 +0100

    Nakon 5.3.

---
+++ 8_Studenti/Zorislav/6_React/V7_Komp/komp/src/Components/WelcomeClass.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class WelcomeClass extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+
+    // inicijalizacija...
+  }
+
+  render() {
+    // isvrsava se kod svakog prikazivanja
+    return <h2>Welcome! Komponenta definirana klasom</h2>;
+  }
+}
