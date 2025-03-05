5d4cf3bb81ec7a2121e3173e0fbdcc3a723b879b
ZokvicM <zokvicmihovil@gmail.com>
2025-03-05 20:09:17 +0100

    Predavanje

---
+++ 8_Studenti/ZokvicM/Razvojna_okolina/V9_jQueryOne/index.js
@@ -0,0 +1,55 @@
+const source = document.getElementById("hb-template").innerHTML;
+const destination = document.getElementById("hb-result");
+const template = Handlebars.compile(source);
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  // Dohvati pokemone
+
+  async function getPokemons() {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-color/yellow"
+    );
+    const responseData = await response.json();
+    return responseData.pokemon_species.slice(0, 20); 
+  }
+
+  // Prikazi pokemone
+
+  function fillList(pokemons) {
+    const context = {
+      pokemon: pokemons,
+    };
+    const html = template(context);
+    destination.innerHTML = html;
+    $('[data-toggle="popover"]').popover();
+
+    afterRender();
+  }
+
+  function afterRender() {
+    $("table th").css("color", "darkBlue");
+    addStripes();
+
+    //Nakon 2s brišemo sve pokemone koji počinju sa slovom p
+
+    setTimeout(function () {
+      const hideElements = $("table td a:contains('p')").filter(function () {
+        //return true ili false
+        return this.innerHTML.indexOf("p") == 0;
+      });
+      hideElements.closest("tr").remove();
+      addStripes();
+    }, 2000);
+  }
+
+  function addStripes() {
+    $("table tr").removeClass("striped");
+    $("table tr:nth-child(even)").addClass("striped");
+  }
+
+  getPokemons().then((pkmns) => {
+    fillList(pkmns);
+  });
+}
