7a7708cbd7eac8a358751836a343061abb09b7d8
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2025-03-08 12:40:31 +0100

    s

--- 8_Studenti/Juraj/6_React/V8_Props/props/src/user/UserFunction.jsx
+++ 8_Studenti/Juraj/6_React/V8_Props/props/src/user/UserFunction.jsx
@@ -1,17 +1,6 @@
-export default function UserFunction({ime, years}) {
-   
-   const { ime, years } = props;
-   
+function UserFunction({name, years}) {
 
-    return <p>Pozdrav moje ime je {ime} i imam {years} godina</p>;
-
-}
-
-// export default function UserFunction({user}) {
-   
-//  const { name, years } = user;
-       
-    
-//         return <p>Pozdrav moje ime je {name} i imam {years} godina</p>;
-    
-//     }
+    return <p>Pozdrav, moje ime je {name} i imam {years} godina.</p>;
+  }
+  
+  export default UserFunction;
