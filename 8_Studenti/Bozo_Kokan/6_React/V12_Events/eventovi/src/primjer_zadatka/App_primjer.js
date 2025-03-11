998cc37208aa45bc1e350a853b784647885553b2
Božo Kokan <bozokokan11@hotmail.com>
2025-03-11 20:59:39 +0100

    predavanje

---
+++ 8_Studenti/Bozo_Kokan/6_React/V12_Events/eventovi/src/primjer_zadatka/App_primjer.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+import Child from './Child';
+
+function App() {
+
+  const messageFromParent = "Budi dobar";
+  const [counter,setCounter] = useState(0);
+  const [childText, setChildText] = useState("");
+
+  function onClickHandler(){
+    setCounter((prevState) => prevState+1);
+  }
+  function changeChildText(msg){
+    setChildText(msg);
+  }
+
+  return (
+    <div className="App">
+      <p>Counter: {counter}</p>
+      <button onClick = {onClickHandler}>Increment counter</button>
+      <h1> This is parent</h1>
+      <h3> Message from child: {childText}</h3>
+      <Child parentMsg = {messageFromParent} parentFn = {changeChildText}/>
+    </div>
+  );
+}
+
+export default App;
