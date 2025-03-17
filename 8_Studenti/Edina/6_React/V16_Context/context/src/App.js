2217d5f3a5c4f2d7364b3378befac9bffe36e3e0
EdinaKolakovic <edina.kolakovic98@gmail.com>
2025-03-17 20:59:35 +0100

    novi commit

--- 8_Studenti/Edina/6_React/V16_Context/context/src/App.js
+++ 8_Studenti/Edina/6_React/V16_Context/context/src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Komponenta1 from "./components/Komponenta1";
 import "./App.css";
 import { Provider } from "./context/context";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      <Provider value={{ text: "Komponenta 4 je najbolja" }}>
+      <Provider value={{ text: "Komponenta 4 je najbolja", number: number }}>
         <h1>App komponenta</h1>
         <Komponenta1 />
       </Provider>
--- 8_Studenti/Edina/6_React/V16_Context/context/src/components/Komponenta4.jsx
+++ 8_Studenti/Edina/6_React/V16_Context/context/src/components/Komponenta4.jsx
@@ -5,12 +5,14 @@ import Context from "../context/context";
 export default function Komponenta4() {
   const mojKontekst = useContext(Context);
 
-  const { text } = mojKontekst;
+  const { text, number } = mojKontekst;
+
+  const mojText = `${text} i ${number} su poslani preko useContext.`;
   return (
     <>
       <h2>Komponenta 4</h2>
       <Consumer>{(ctx) => <p>{ctx.text}</p>}</Consumer>
-      <p>{text}</p>
+      <p>{mojText}</p>
     </>
   );
 }
