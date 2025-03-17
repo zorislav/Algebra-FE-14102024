083b5ae9b184e8004e1e65ade24d99d28fd683d0
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-03-17 21:16:07 +0100

    Nakon 17.3.

--- 8_Studenti/Zorislav/6_React/V16_Context/context/src/App.js
+++ 8_Studenti/Zorislav/6_React/V16_Context/context/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Komponenta1 from "./components/Komponenta1";
+import "./App.css";
+import { Provider } from "./context/context";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  function numberChangeHandler(event) {
+    setNumber(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider value={{ text: "Komponenta 4 je najbolja", number: number, reset: ()=>setNumber(0) }}>
+        <h1>App Komponenta</h1>
+        <label htmlFor="broj">Postavi u kontekst:</label>
+        <input
+          id="broj"
+          value={number}
+          type="number"
+          onChange={numberChangeHandler}
+        />
+        <Komponenta1 />
+      </Provider>
     </div>
   );
 }
