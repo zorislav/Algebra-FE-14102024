29c7b4f50c274a65b0f5e829a5c206230e5e072c
EdinaKolakovic <edina.kolakovic98@gmail.com>
2025-03-17 21:13:03 +0100

    novi

--- 8_Studenti/Edina/6_React/V16_Context/context/src/App.js
+++ 8_Studenti/Edina/6_React/V16_Context/context/src/App.js
@@ -6,10 +6,27 @@ import { Provider } from "./context/context";
 function App() {
   const [number, setNumber] = useState(0);
 
+  function numberChangeHandler(event) {
+    setNumber(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Provider value={{ text: "Komponenta 4 je najbolja", number: number }}>
+      <Provider
+        value={{
+          text: "Komponenta 4 je najbolja",
+          number: number,
+          reset: () => setNumber(0),
+        }}
+      >
         <h1>App komponenta</h1>
+        <label htmlFor="broj">Postavi u kontekst: </label>
+        <input
+          id="broj"
+          value={number}
+          type="number"
+          onChange={numberChangeHandler}
+        ></input>
         <Komponenta1 />
       </Provider>
     </div>
--- 8_Studenti/Edina/6_React/V16_Context/context/src/components/Komponenta4.jsx
+++ 8_Studenti/Edina/6_React/V16_Context/context/src/components/Komponenta4.jsx
@@ -5,7 +5,7 @@ import Context from "../context/context";
 export default function Komponenta4() {
   const mojKontekst = useContext(Context);
 
-  const { text, number } = mojKontekst;
+  const { text, number, reset } = mojKontekst;
 
   const mojText = `${text} i ${number} su poslani preko useContext.`;
   return (
@@ -13,6 +13,7 @@ export default function Komponenta4() {
       <h2>Komponenta 4</h2>
       <Consumer>{(ctx) => <p>{ctx.text}</p>}</Consumer>
       <p>{mojText}</p>
+      <button onClick={reset}>Reset</button>
     </>
   );
 }
