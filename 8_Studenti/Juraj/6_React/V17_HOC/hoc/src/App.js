a9ec899177767a2994d8acde1cd3e195f4f45d86
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2025-03-19 19:03:44 +0100

    hoc

--- 8_Studenti/Juraj/6_React/V17_HOC/hoc/src/App.js
+++ 8_Studenti/Juraj/6_React/V17_HOC/hoc/src/App.js
@@ -1,22 +1,35 @@
 
-const withColor = (Element) => ({color, ...otherProps}) => (
-  <Element {...otherProps} style={{backgroundColor: color}} />
-);
+import './App.css';
 
-const Button = (props) => {
-  return <button {...props}></button>;
+function Button(props) {
+  return <button {...props}></button>
 }
 
-const ColoredButton = withColor(Button);
+function withColor(Element) {
+  return function({boja, ...otherProps}){
+    return <Element {...otherProps} style={{backgroundColor: boja}} />
+  }
+};
+
+const ColoredElement = withColor(Button)
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-      <ColoredButton color="red">Ja sam crven</ColoredButton>
-      <ColoredButton color="yellow">Ja sam zut</ColoredButton>
-    </>
+    <div className="App">
+     <h1 style={styles.h1stil}>Hello</h1>
+     <h1 style={{color: "yellow", backgroundColor: "green"}}>Hello again</h1>
+     <ColoredElement boja="red">Ja sam crven</ColoredElement>
+      <ColoredElement boja="green">Ja sam zelen</ColoredElement>
+    </div>
   );
 }
 
 export default App;
+
+
+const styles = {
+  h1stil: {
+    color: "blue", 
+    backgroundColor: "lightGray",
+  },
+};
