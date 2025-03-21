0ac83d20ea6e107a41ce6afbf18b0b4ed3f6e2ee
Božo Kokan <bozokokan11@hotmail.com>
2025-03-21 20:15:40 +0100

    preedavanje

---
+++ 8_Studenti/Bozo_Kokan/6_React/V19_API/api/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useEffect, useState } from "react";
+
+const NASA_API_KEY = 'F7NDSvVq6xtFLM8IDjYHPEd7bG0GzdVH501agwwn';
+
+function App() {
+
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    const response  = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`);
+    const responseData = await response.json();
+    setData(responseData);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[]);
+
+  const renderContent = data 
+  ? (
+    <>      
+      <h1>NASA Picture of the day</h1>
+      <h3>{data.title}</h3>
+      <img src={data.url} alt={data.title} width={500}></img>
+      <p>{data.explanation}</p>      
+    </>
+  ) 
+  : (
+    <>
+        <p>Loading NASA Data..</p>
+    </>
+  );
+  
+
+  return (
+    <div className="App">
+      {renderContent}
+    </div>
+  );
+}
+
+export default App;
