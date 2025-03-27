16059da1fe5a118d2d0934918ca496cc5819cba1
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2025-03-27 19:19:24 +0100

    s

--- 8_Studenti/Juraj/6_React/V22_Redux_Thunk/redux-thnk/src/App.js
+++ 8_Studenti/Juraj/6_React/V22_Redux_Thunk/redux-thnk/src/App.js
@@ -1,53 +1,28 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useDispatch, useSelector} from "react-redux"
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getNasaData } from './store/redux-store';
 
 function  App() {
 
-  const [data, setData] = useState(null);
+  const nasaData = useSelector(state => state.nasaData);
   const dispatch = useDispatch();
 
-  // Opcija 3
-  const getData = useCallback (async () => {
-    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=F7NDSvVq6xtFLM8IDjYHPEd7bG0GzdVH501agwwn');
-
-    const responseData = await response.json();
-    setData(responseData);
-  }, []);
-
   useEffect(() => {
-    
-    // Opcija 1
-    // fetch('https://api.nasa.gov/planetary/apod?api_key=F7NDSvVq6xtFLM8IDjYHPEd7bG0GzdVH501agwwn')
-    //   .then(response => response.json().then(resObj => {
-    //     setData(resObj);
-    //   }));
-    
-    // Opcija 2
-    // const getData = async () => {
-    //   const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=F7NDSvVq6xtFLM8IDjYHPEd7bG0GzdVH501agwwn');
-    //   const responseData = await response.json();
-    //   setData(responseData);
-    // };
-
-    getData();
     dispatch(getNasaData());
   }, [dispatch]);
- 
 
-  if(!data){
+  if(!nasaData.title){
     return <p>Loading NASA data...</p>;
   }
 
   return (
     <>
       <h1>NASA picture of the day</h1>
-      <h3>{data.title}</h3>
-      <img src={data.url} alt={data.title} width={500} />
-      <p>{data.explanation}</p>
+      <h3>{nasaData.title}</h3>
+      <img src={nasaData.url} alt={nasaData.title} width={500} />
+      <p>{nasaData.explanation}</p>
     </>
   );
-  
 
 }
 
