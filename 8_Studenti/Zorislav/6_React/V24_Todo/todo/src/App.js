34d6158f3608520224546149a19a293ad8c9d628
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-03-27 21:06:23 +0100

    Nakon 27.3.

--- 8_Studenti/Zorislav/6_React/V24_Todo/todo/src/App.js
+++ 8_Studenti/Zorislav/6_React/V24_Todo/todo/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { VisibilityToolbar, AddTodoForm, TodoList } from "./components";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: ["prvi", "drugi"],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="header">My tasks</h1>
+        <VisibilityToolbar />
+        <div className="todo-container">
+          <AddTodoForm />
+          <TodoList todos={todos} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
