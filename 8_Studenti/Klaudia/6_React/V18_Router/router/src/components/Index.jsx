8ff60744bd9c10b8d2d144eac41a9d83c544da91
Klaludia <klaudija15@windowslive.com>
2025-03-27 17:08:34 +0100

    -27.3.

---
+++ 8_Studenti/Klaudia/6_React/V18_Router/router/src/components/Index.jsx
@@ -0,0 +1,10 @@
+export default function Index(){
+
+    return (
+        <>
+        <h1>Welcome to router!</h1>
+        <p>Ovo je primjer korištenja React Router-a.</p>
+        <p>Sad sam na pocetnoj stranici.</p>
+        </>
+    ); 
+}
---
+++ 8_Studenti/Klaudia/6_React/V1_Prva/moja-prva-aplikacija/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function MojaAplikacija() {
+
+  // JS kod
+  
+
+  return (
+    <div className="App">
+      <h1>Moja prva aplikacija</h1>
+    </div>
+  );
+}
+
+export default MojaAplikacija;
