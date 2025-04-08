618e055b6e7b9aa77692d7118efb3673fc123a94
Juraj Guic <43215986+Ju1988@users.noreply.github.com>
2025-04-08 18:25:52 +0200

    za

---
+++ 8_Studenti/Juraj/7_Zavrsni/webchat/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component() {
+  render(){
+
+    return (
+      <div className="app">
+       
+        <div className='header'>
+        <h1>My Chat App</h1>
+        </div>
+      </div>
+    );
+
+  }
+  
+  
+}
+
+export default App;
